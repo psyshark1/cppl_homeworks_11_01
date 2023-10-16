65afb1fc444143ad887a65471f4bb5d014b7f403
Andrey <andrey.zalozhenkov@pikta.ru>
2023-10-16 10:05:30 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+template<typename T>
+void move_vectors(std::vector<T>& lhs, std::vector<T>& rhs)
+{
+	rhs = std::move(lhs);
+}
+
+int main()
+{
+	std::vector<std::string> one = { "test_string1", "test_string2" };
+	std::vector<std::string> two;
+
+	move_vectors(one, two);
+	return 0;
+}
